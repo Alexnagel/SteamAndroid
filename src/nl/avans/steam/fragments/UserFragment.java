0971d0c9eb916cdbf4dba6de389d54a41ba54741
turbodevin <turbodevin@hotmail.com>
2014-04-09 16:56:46 +0200

    support v4 removed

--- src/nl/avans/steam/fragments/UserFragment.java
+++ src/nl/avans/steam/fragments/UserFragment.java
@@ -3,7 +3,7 @@ package nl.avans.steam.fragments;
 import nl.avans.steam.R;
 import nl.avans.steam.R.layout;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
